05d16f1ded6fac557d380a55aff3a1eeb5fa3dc9
GSCrawley <gscrawley@gmail.com>
2020-12-12 01:37:14 -0800

    challenge 1

---
+++ final_assessment.js
@@ -0,0 +1,5 @@
+function goldenRatio(n) {
+    var gR = 1.61803398875;
+    return n * gR;
+}
+console.log(goldenRatio(7));
---
+++ final_assessment.ts
@@ -0,0 +1,8 @@
+
+function goldenRatio(n: number): number{
+    let gR = 1.61803398875
+    return n * gR;
+}
+
+console.log(goldenRatio(1))
+
